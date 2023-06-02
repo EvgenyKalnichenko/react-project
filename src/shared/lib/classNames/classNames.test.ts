cedfa045c0fb91426144763c3791824183533c25
kalni <kalnichenko.1996@mail.ru>
2023-06-02 17:35:53 +0300

    add jest config, first unit test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,40 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('testClass')).toBe('testClass');
+    });
+
+    test('with additional class', () => {
+        const expected = 'testClass class class-name';
+        expect(classNames('testClass', {}, ['class', 'class-name']))
+            .toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'testClass class class-name hovered scrollable';
+        expect(classNames(
+            'testClass',
+            { hovered: true, scrollable: true },
+            ['class', 'class-name'],
+        )).toBe(expected);
+    });
+
+    test('with mods false', () => {
+        const expected = 'testClass class class-name hovered';
+        expect(classNames(
+            'testClass',
+            { hovered: true, scrollable: false },
+            ['class', 'class-name'],
+        )).toBe(expected);
+    });
+
+    test('with mods undefined', () => {
+        const expected = 'testClass class class-name hovered';
+        expect(classNames(
+            'testClass',
+            { hovered: true, scrollable: undefined },
+            ['class', 'class-name'],
+        )).toBe(expected);
+    });
+});
